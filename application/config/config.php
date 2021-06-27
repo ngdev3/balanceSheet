96ee3a592993fada6d93b076b33e1b56c6426eda
rajatguptaTechlib <rajatinvoice@gmail.com>
2021-06-27 10:22:22 +0530

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if( $_SERVER['HTTP_HOST'] == 'balancesheetsolutions.online'){
     
-    $config['base_url'] = 'https://'.$_SERVER['HTTP_HOST'];
+    $config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'];
 }else{
     
     $config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/RiceMill/balanceSheet';
