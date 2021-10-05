9a058f5687e55610a0aaa2bcfde5be1e1c593bae
ngdev3 <rajat.gupta@intelegencia.com>
2021-10-05 13:00:07 +0530

    no message

--- application/modules/admin/controllers/Report.php
+++ application/modules/admin/controllers/Report.php
@@ -518,6 +518,12 @@ class Report extends CI_Controller {
 		$this->Report_mod->Billing_check();
 	}
 	
+	public function Billing_check_template_6(){
+		if(isPostBack()){
+		}
+		$this->Report_mod->Billing_check_template_6();
+	}
+	
 
 }
 
